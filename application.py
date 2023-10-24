31e0e78f47a12210712b30eaa67e997ee2fcb798
John Blanton <hello@johnblanton.com>
2023-10-24 07:29:55 -0600

    add sitemap generator to application.py

--- application.py
+++ application.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, jsonify, request
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    Response,
+    render_template_string,
+)
 from utils.generator import generate_paragraph
 from dotenv import load_dotenv
 import requests
 import os
+import datetime
 
 application = Flask(__name__)
 
@@ -51,6 +59,40 @@ def get_puppy_image():
         return jsonify(data)
     except Exception as e:
         return jsonify(error=str(e)), 500
+
+
+def generate_sitemap(app):
+    sitemap_template = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    {% for route in routes %}
+    <url>
+        <loc>{{ url_root }}{{ route }}</loc>
+        <lastmod>{{ lastmod }}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    {% endfor %}
+</urlset>
+    """
+    routes = []
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and len(rule.arguments) == 0:
+            url = f"{rule.rule}"
+            routes.append(url)
+
+    sitemap_xml = render_template_string(
+        sitemap_template,
+        routes=routes,
+        lastmod=datetime.datetime.now().date(),
+        url_root=request.url_root,
+    )
+    return sitemap_xml
+
+
+@application.route("/sitemap.xml")
+def sitemap():
+    sitemap_xml = generate_sitemap(application)
+    return Response(sitemap_xml, mimetype="application/xml")
 
 
 if __name__ == "__main__":
